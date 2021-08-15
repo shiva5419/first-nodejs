dd7c770758dd6e65f6ad0456a0bc046368bf850f
shiva prakash <sivaprakashocs@gmail.com>
2021-08-15 12:54:15 +0530

    to route changes

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('index.js files');
+const axios = require('axios');
+const express = require('express');
+
+let app = express();
+const port = process.env.PORT || 9999;
+
+app.get('/',(req, res) => {
+	res.send(`<h1> Hello World</h1>`);
+});
+app.get('/about',(req, res) => {
+	res.send(`<h1> Hello About this Page</h1>`);
+});
+app.get('/service',(req, res) => {
+	res.send(`<h1> Hello Service this page</h1>`);
+});
+
+app.get('/service/:id',(req, res) => {
+	res.send(`<h1> Hello Service this page ${req.params.id}</h1>`);
+});
+
+app.get('/service/:id/page/:page',(req, res) => {
+	res.send(`<h1> Hello Service this page ${req.params.id} - ${req.params.page}</h1>`);
+});
+
+
+app.listen(port);
+console.log('server connection'+port);
