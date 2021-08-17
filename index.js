5143b847fa2adc2098ccdc87243cfa4ee32624d0
shiva prakash <sivaprakashocs@gmail.com>
2021-08-17 08:29:16 +0530

    post services

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const axios = require('axios');
 const express = require('express');
-
+const bodyParser = require('body-parser');
 let app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended : false}))
+
 const port = process.env.PORT || 9999;
 
 app.get('/',(req, res) => {
@@ -22,6 +25,15 @@ app.get('/service/:id/page/:page',(req, res) => {
 	res.send(`<h1> Hello Service this page ${req.params.id} - ${req.params.page}</h1>`);
 });
 
+app.use((req, res, next) => {
+	console.log('middleware');
+	next();
+});
+
+app.use('/post', express.static(__dirname+'/views/form/form.html'))
+app.post('/postservice',(req, res) => {
+	res.send(req.body);
+});
 
 app.listen(port);
 console.log('server connection'+port);
